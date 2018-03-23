e8e98c1bc7d5cefb5cef6c73ea5366c315dae6b0
ABHAY NIGAM <MASHUK999@REDIFFMAIL.COM>
2018-03-23 20:50:08 +0530

    Second Commit

--- app/src/main/java/vit/org/vitapp/MainActivity.java
+++ app/src/main/java/vit/org/vitapp/MainActivity.java
@@ -2,12 +2,41 @@ package vit.org.vitapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView first;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
         setContentView(R.layout.activity_main);
+
+        first = (WebView) findViewById(R.id.site);
+       // WebSettings webSettings = first.getSettings();
+        //webSettings.setJavaScriptEnabled(true);
+        first.loadUrl("http://www.google.com");
+        first.setWebViewClient(new WebViewClient());
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if(first.canGoBack())
+        {
+            first.goBack();
+        }
+        else {
+            super.onBackPressed();
+        }
     }
 }
